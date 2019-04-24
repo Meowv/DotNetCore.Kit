79eb77b99c3490ed35be1f1c240d3e2fd3be1e9a
qix <hackxing@foxmail.com>
2019-04-24 18:19:02 +0800

    AppendIf

---
+++ src/DotNetCore.Kit/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCore.Kit
+{
+    public static class Extensions
+    {
+        public static StringBuilder AppendIf<T>(this StringBuilder @this, Func<T, bool> predicate, params T[] values)
+        {
+            foreach (T val in values)
+            {
+                if (predicate(val))
+                {
+                    @this.Append(val);
+                }
+            }
+            return @this;
+        }
+    }
+}
