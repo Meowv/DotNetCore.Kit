6a3570351d14ec31bde5ee410240b0a351dc1261
qix <hackxing@foxmail.com>
2019-04-25 14:55:06 +0800

    EnsureStartsWith

--- src/DotNetCore.Kit/Extensions.cs
+++ src/DotNetCore.Kit/Extensions.cs
@@ -599,6 +599,58 @@ namespace DotNetCore.Kit
             return stringBuilder.ToString();
         }
 
+        /// <summary>
+        /// EnsureStartsWith
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static string EnsureStartsWith(this string str, char c)
+        {
+            return str.EnsureStartsWith(c, StringComparison.InvariantCulture);
+        }
+
+        /// <summary>
+        /// EnsureStartsWith
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="c"></param>
+        /// <param name="comparisonType"></param>
+        /// <returns></returns>
+        public static string EnsureStartsWith(this string str, char c, StringComparison comparisonType)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException("str");
+            }
+            if (str.StartsWith(c.ToString(CultureInfo.InvariantCulture), comparisonType))
+            {
+                return str;
+            }
+            return c.ToString() + str;
+        }
+
+        /// <summary>
+        /// EnsureStartsWith
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="c"></param>
+        /// <param name="ignoreCase"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public static string EnsureStartsWith(this string str, char c, bool ignoreCase, CultureInfo culture)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException("str");
+            }
+            if (str.StartsWith(c.ToString(culture), ignoreCase, culture))
+            {
+                return str;
+            }
+            return c.ToString() + str;
+        }
+
 
         /// <summary>
         /// IfTrue
