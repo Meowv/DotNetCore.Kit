9cdb5d1acb3195ba23d937f848d6b73e5f2e3534
qix <hackxing@foxmail.com>
2019-04-25 09:12:22 +0800

    AppendLineFormat、AppendLineIf

--- src/DotNetCore.Kit/Extensions.cs
+++ src/DotNetCore.Kit/Extensions.cs
@@ -6,6 +6,14 @@ namespace DotNetCore.Kit
 {
     public static class Extensions
     {
+        /// <summary>
+        /// AppendIf<T>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="this"></param>
+        /// <param name="predicate"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
         public static StringBuilder AppendIf<T>(this StringBuilder @this, Func<T, bool> predicate, params T[] values)
         {
             foreach (T val in values)
@@ -13,6 +21,53 @@ namespace DotNetCore.Kit
                 if (predicate(val))
                 {
                     @this.Append(val);
+                }
+            }
+            return @this;
+        }
+
+        /// <summary>
+        /// AppendLineFormat
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="format"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static StringBuilder AppendLineFormat(this StringBuilder @this, string format, List<IEnumerable<object>> args)
+        {
+            @this.AppendLine(string.Format(format, args));
+            return @this;
+        }
+
+        /// <summary>
+        /// AppendLineFormat
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="format"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static StringBuilder AppendLineFormat(this StringBuilder @this, string format, params object[] args)
+        {
+            @this.AppendLine(string.Format(format, args));
+            return @this;
+        }
+
+        /// <summary>
+        /// AppendLineIf<T>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="this"></param>
+        /// <param name="predicate"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public static StringBuilder AppendLineIf<T>(this StringBuilder @this, Func<T, bool> predicate, params T[] values)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                T arg = values[i];
+                if (predicate(arg))
+                {
+                    @this.AppendLine(arg.ToString());
                 }
             }
             return @this;
