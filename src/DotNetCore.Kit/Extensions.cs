d40e6668936359dfa70136d2cdb33bb9bb6f4c00
qix <hackxing@foxmail.com>
2019-04-25 15:14:48 +0800

    FormatWith

--- src/DotNetCore.Kit/Extensions.cs
+++ src/DotNetCore.Kit/Extensions.cs
@@ -734,6 +734,53 @@ namespace DotNetCore.Kit
         }
 
         /// <summary>
+        /// FormatWith
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public static string FormatWith(this string @this, params object[] values)
+        {
+            return string.Format(@this, values);
+        }
+
+        /// <summary>
+        /// FormatWith
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="arg0"></param>
+        /// <param name="arg1"></param>
+        /// <param name="arg2"></param>
+        /// <returns></returns>
+        public static string FormatWith(this string @this, object arg0, object arg1, object arg2)
+        {
+            return string.Format(@this, arg0, arg1, arg2);
+        }
+
+        /// <summary>
+        /// FormatWith
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="arg0"></param>
+        /// <param name="arg1"></param>
+        /// <returns></returns>
+        public static string FormatWith(this string @this, object arg0, object arg1)
+        {
+            return string.Format(@this, arg0, arg1);
+        }
+
+        /// <summary>
+        /// FormatWith
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="arg0"></param>
+        /// <returns></returns>
+        public static string FormatWith(this string @this, object arg0)
+        {
+            return string.Format(@this, arg0);
+        }
+
+        /// <summary>
         /// ToInt
         /// </summary>
         /// <param name="expression"></param>
