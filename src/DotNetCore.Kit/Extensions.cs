66daeb111fcd0cb6ed0654e5054e329935ea83f1
qix <hackxing@foxmail.com>
2019-04-26 16:14:41 +0800

    TrimEnd

--- src/DotNetCore.Kit/Extensions.cs
+++ src/DotNetCore.Kit/Extensions.cs
@@ -2674,6 +2674,29 @@ namespace DotNetCore.Kit
             }
         }
 
+        /// <summary>
+        /// TrimEnd
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="forRemoving"></param>
+        /// <returns></returns>
+        public static string TrimEnd(this string value, string forRemoving)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+            if (string.IsNullOrEmpty(forRemoving))
+            {
+                return value;
+            }
+            while (value.EndsWith(forRemoving, StringComparison.InvariantCultureIgnoreCase))
+            {
+                value = value.Remove(value.LastIndexOf(forRemoving, StringComparison.InvariantCultureIgnoreCase));
+            }
+            return value;
+        }
+
         #region Private
 
         /// <summary>
