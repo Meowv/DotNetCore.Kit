1a774edee193e0dae03bac806abded02dc00398c
qix <hackxing@foxmail.com>
2019-04-28 10:35:42 +0800

    DistinctBy:根据集合中指定的列过滤重复

--- src/DotNetCore.Kit/Extensions.cs
+++ src/DotNetCore.Kit/Extensions.cs
@@ -491,6 +491,26 @@ namespace DotNetCore.Kit
         }
 
         /// <summary>
+        /// DistinctBy:根据集合中指定的列过滤重复
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="where"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> DistinctBy<T, TResult>(this IEnumerable<T> source, Func<T, TResult> where)
+        {
+            HashSet<TResult> hashSetData = new HashSet<TResult>();
+            foreach (T item in source)
+            {
+                if (hashSetData.Add(where(item)))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        /// <summary>
         /// Elapsed
         /// </summary>
         /// <param name="startTime"></param>
