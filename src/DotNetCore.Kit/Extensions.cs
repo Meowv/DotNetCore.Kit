67bc245e5e2d814d6885deef6e70dfb7f524d268
qix <hackxing@foxmail.com>
2019-04-28 10:15:30 +0800

    GenerateTree:列表生成树形节点

--- src/DotNetCore.Kit/Extensions.cs
+++ src/DotNetCore.Kit/Extensions.cs
@@ -1086,6 +1086,34 @@ namespace DotNetCore.Kit
         }
 
         /// <summary>
+        /// GenerateTree:列表生成树形节点
+        /// </summary>
+        /// <typeparam name="T">集合对象的类型</typeparam>
+        /// <typeparam name="K">父节点的类型</typeparam>
+        /// <param name="collection">集合</param>
+        /// <param name="idSelector">主键ID</param>
+        /// <param name="parentIdSelector">父节点</param>
+        /// <param name="rootId">根节点</param>
+        /// <returns></returns>
+        public static IEnumerable<TreeItem<T>> GenerateTree<T, K>(this IEnumerable<T> collection, Func<T, K> idSelector,
+            Func<T, K> parentIdSelector, K rootId = default)
+        {
+            foreach (var c in collection.Where(u =>
+            {
+                var selector = parentIdSelector(u);
+                return (rootId == null && selector == null)
+                       || (rootId != null && rootId.Equals(selector));
+            }))
+            {
+                yield return new TreeItem<T>
+                {
+                    Item = c,
+                    Children = collection.GenerateTree(idSelector, parentIdSelector, idSelector(c))
+                };
+            }
+        }
+
+        /// <summary>
         /// HtmlDecode
         /// </summary>
         /// <param name="s"></param>
@@ -2947,6 +2975,27 @@ namespace DotNetCore.Kit
                 return dt.ToString(includeTime ? "MM-dd HH:mm" : "MM-dd");
             }
             return dt.ToString(includeTime ? "yyyy-MM-dd HH:mm" : "yyyy-MM-dd");
+        }
+
+        #endregion
+
+        #region Common class
+
+        /// <summary>
+        /// TreeItem
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public class TreeItem<T>
+        {
+            /// <summary>
+            /// Item
+            /// </summary>
+            public T Item { get; set; }
+
+            /// <summary>
+            /// Children
+            /// </summary>
+            public IEnumerable<TreeItem<T>> Children { get; set; }
         }
 
         #endregion
