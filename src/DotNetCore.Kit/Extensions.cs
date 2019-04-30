c1f14980747375bc9bcac45003e85d67cd97f8d5
qix <hackxing@foxmail.com>
2019-04-30 10:23:53 +0800

    ToExpando

--- src/DotNetCore.Kit/Extensions.cs
+++ src/DotNetCore.Kit/Extensions.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Dynamic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -2917,6 +2918,47 @@ public static class Extensions
             table.Rows.Add(row);
         }
         return table;
+    }
+
+    /// <summary>
+    /// ToExpando
+    /// </summary>
+    /// <param name="dictionary"></param>
+    /// <returns></returns>
+    public static ExpandoObject ToExpando(this IDictionary<string, object> dictionary)
+    {
+        ExpandoObject expandoObject = new ExpandoObject();
+        IDictionary<string, object> dictionary2 = expandoObject;
+        foreach (KeyValuePair<string, object> item2 in dictionary)
+        {
+            if (item2.Value is IDictionary<string, object>)
+            {
+                ExpandoObject value = ((IDictionary<string, object>)item2.Value).ToExpando();
+                dictionary2.Add(item2.Key, value);
+            }
+            else if (item2.Value is ICollection)
+            {
+                List<object> list = new List<object>();
+                foreach (object item3 in (ICollection)item2.Value)
+                {
+                    if (item3 is IDictionary<string, object>)
+                    {
+                        ExpandoObject item = ((IDictionary<string, object>)item3).ToExpando();
+                        list.Add(item);
+                    }
+                    else
+                    {
+                        list.Add(item3);
+                    }
+                }
+                dictionary2.Add(item2.Key, list);
+            }
+            else
+            {
+                dictionary2.Add(item2);
+            }
+        }
+        return expandoObject;
     }
 
     /// <summary>
