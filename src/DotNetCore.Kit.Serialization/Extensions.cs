890794803fbef783f19be85e81ad59439ca387f7
qix <hackxing@foxmail.com>
2019-05-08 09:21:31 +0800

    SerializeBinary

---
+++ src/DotNetCore.Kit.Serialization/Extensions.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+/// <summary>
+/// Serialization.Extensions
+/// </summary>
+public static class Extensions
+{
+    /// <summary>
+    /// SerializeBinary
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="this"></param>
+    /// <returns></returns>
+    public static string SerializeBinary<T>(this T @this)
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        using (MemoryStream memoryStream = new MemoryStream())
+        {
+            binaryFormatter.Serialize(memoryStream, @this);
+            return Encoding.Default.GetString(memoryStream.ToArray());
+        }
+    }
+
+    /// <summary>
+    /// SerializeBinary
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="this"></param>
+    /// <param name="encoding"></param>
+    /// <returns></returns>
+    public static string SerializeBinary<T>(this T @this, Encoding encoding)
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        using (MemoryStream memoryStream = new MemoryStream())
+        {
+            binaryFormatter.Serialize(memoryStream, @this);
+            return encoding.GetString(memoryStream.ToArray());
+        }
+    }
+}
